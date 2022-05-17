696e1642cb13a1b8427cd6962356767cfd655807
Josh Bennett <josh.bennett77@yahoo.com>
2022-05-17 15:54:56 -0600

    Grid population functionality added
    Wrote a function (createGrid) that uses DOM manipulation to add divs to a container
    and change the style of individual grid squares and arrange those divs
    equally, using CSS Grid. Another function (getGridRangeValue) gets the
    value of the range input element and assigns that to a variable that is
    then called as the argument for how many square divs to create.

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-//function createGrid(input) {
-//	let grid = document.querySelector(.)
-//}
+const grid = document.querySelector("#grid-box");
+let range = document.getElementById("grid-size");
+
+
+function createGrid(value) {
+	console.log(value);
+  for (let i = 0; i < value * value; i++) {
+		const gridSquare = document.createElement("div");
+    gridSquare.className = "grid-square";
+    gridSquare.textContent = "";
+    grid.appendChild(gridSquare);
+  }
+	
+  grid.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${value}), 1fr)`;
+};
+
+function getGridRangeValue() {
+	let rangeValue = range.value;
+	return parseInt(rangeValue);
+}
+
+createGrid(getGridRangeValue());
