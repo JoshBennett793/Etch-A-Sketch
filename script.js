b93f6c72dac7071792780871ed41a9abf8a07b3b
Josh Bennett <josh.bennett77@yahoo.com>
2022-05-17 22:27:38 -0600

    Added event listener to range input element
    Event listener listens for "input", subsequently updating the grid as
    user is traversing the range input slider. I also added a range input
    "display" div and added a feature to the createGrid function that
    updates the DOM with the new range value.

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const grid = document.querySelector("#grid-box");
+const gridSquare = document.querySelector(".grid-square");
 let range = document.getElementById("grid-size");
-
+let rangeInput = document.querySelector("#range-input");
 
 function createGrid(value) {
-	console.log(value);
   for (let i = 0; i < value * value; i++) {
-		const gridSquare = document.createElement("div");
-    gridSquare.className = "grid-square";
-    gridSquare.textContent = "";
-    grid.appendChild(gridSquare);
+		rangeInput.textContent = `${parseInt(range.value)} x ${parseInt(range.value)}`;
+    const gridSquareDiv = document.createElement("div");
+    gridSquareDiv.className = "grid-square";
+    gridSquareDiv.textContent = "";
+    grid.appendChild(gridSquareDiv);
   }
-	
+
   grid.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${value}), 1fr)`;
-};
-
-function getGridRangeValue() {
-	let rangeValue = range.value;
-	return parseInt(rangeValue);
 }
 
-createGrid(getGridRangeValue());
+createGrid(parseInt(range.value));
+
+range.addEventListener("input", () => {
+  grid.textContent = "";
+  createGrid(parseInt(range.value));
+});
