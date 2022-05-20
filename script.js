478444b2716a6c07af91055c66907f30b773e261
Josh Bennett <josh.bennett77@yahoo.com>
2022-05-20 00:18:09 -0600

    Added radio input "checked" switching feature
    Radio inputs uncheck previously checked radio input "onchange".

--- script.js
+++ script.js
@@ -24,9 +24,8 @@ const button = document.querySelector("#clear");
 
 function createGrid(value) {
   for (let i = 0, j = 1; i < value * value; i++, j++) {
-    rangeInput.textContent = `${parseInt(range.value)} x ${parseInt(
-      range.value
-    )}`;
+    rangeInput.textContent =
+     `${parseInt(range.value)} x ${parseInt(range.value)}`;
     const gridSquareDiv = document.createElement("div");
     gridSquareDiv.className = `grid-square grid-square${j}`;
     gridSquareDiv.setAttribute("onMouseEnter", "handleMouseEnter(event)");
@@ -38,6 +37,16 @@ function createGrid(value) {
   grid.style.gridTemplateRows = `repeat(${value}), 1fr)`;
 }
 
+function handleMouseEnter(event) {
+  if (eraserRadio.checked === false) {
+    event.target.style.backgroundColor = `${color}`;
+    console.log(randomRadio.checked, greyscaleRadio.checked, eraserRadio.checked);
+  } else {
+      event.target.style.backgroundColor = "white";
+      console.log(randomRadio.checked, greyscaleRadio.checked, eraserRadio.checked);
+  }
+}
+
 createGrid(parseInt(range.value));
 
 range.addEventListener("input", () => {
@@ -47,18 +56,40 @@ range.addEventListener("input", () => {
 
 paintColor.addEventListener("input", () => {
   color = paintColor.value;
+  randomRadio.checked = false;
+  greyscaleRadio.checked = false;
+  eraserRadio.checked = false;
 });
 
-function handleMouseEnter(event) {
-  let eraserRadioValue = document.querySelector("input[name=eraser]").checked;
+paintColor.addEventListener("click", () => {
+  eraserRadio.checked = false;
+});
 
-  if (eraserRadioValue === true ) {
-    event.target.style.backgroundColor = "white";
-  } else {
-    event.target.style.backgroundColor = `${color}`;
+const randomRadio = document.querySelector("input[name=random]");
+const greyscaleRadio = document.querySelector("input[name=greyscale]");
+const eraserRadio = document.querySelector("input[name=eraser]");
+  
+randomRadio.addEventListener("change", () => {
+  if (randomRadio.checked === true) {
+    greyscaleRadio.checked = false;
+    eraserRadio.checked = false;
   }
-};
-
+});
+  
+greyscaleRadio.addEventListener("change", () => {
+  if (greyscaleRadio.checked === true) {
+    randomRadio.checked = false;
+    eraserRadio.checked = false;
+  }
+});
+  
+eraserRadio.addEventListener("change", () => {
+  if (eraserRadio.checked === true) {
+    randomRadio.checked = false;
+    greyscaleRadio.checked = false;
+  }
+});
+  
 button.addEventListener("click", () => {
   grid.textContent = "";
   createGrid(parseInt(range.value));
